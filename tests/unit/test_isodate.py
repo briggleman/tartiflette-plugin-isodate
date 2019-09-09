0e1889f1dad5df952e1dafa28156db8bbb69d41d
briggleman <ben.riggleman@gmail.com>
2019-09-09 16:03:02 -0400

    fix: corrected import name

--- tests/unit/test_isodate.py
+++ tests/unit/test_isodate.py
@@ -3,6 +3,6 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_isodate_bake():
-    from tartiflette_plugin_isodate import bake, _SDL
+    from tartiflette_plugin_isodatenow import bake, _SDL
 
     assert await bake("a", {}) == _SDL
